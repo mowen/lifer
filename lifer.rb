9a0cff1cc9e8ddbc1df716d61b35203919cd6523
Martin Owen <martinowenuk@gmail.com>
2009-11-11 22:31:13 +0000

    Changed to use simple-rss so that I can read Atom feeds. I've added some but they aren't working yet. Changed templates to output the stream as a list. Now sorting the stream, and restricting it to a maximum number of items.

--- lifer.rb
+++ lifer.rb
@@ -1,6 +1,6 @@
 require "erb"
 require "open-uri"
-require "rss"
+require "simple-rss"
 require "yaml"
 
 module Lifer
@@ -16,42 +16,60 @@ module Lifer
       end
       eval(assignments)
       
-      endpoint = eval('"' + unexpanded_endpoint + '"')
-
-      @rss_content = get(endpoint)
+      @endpoint = eval('"' + unexpanded_endpoint + '"')
     end
     
-    def get(endpoint)
+    def get
       rss_feed = ""
-      open(endpoint) do |raw_feed|
+      open(@endpoint) do |raw_feed|
         rss_feed = raw_feed.read
       end
-      rss_content = RSS::Parser.parse(rss_feed, false)
+      rss_content = SimpleRSS.parse(rss_feed)
       rss_content
     end
     
-    def render
-      items = @rss_content.items
-      template = ""
-      File.open(items_template_filename, "r").each do |line|
-        template << line
-      end
-      erb = ERB.new(template)
-      erb.result(binding)
-    end
-    
-    def items_template_filename
-      feed_specific_filename = File.join("templates", "#{@name}.rhtml")
-      if File.exists?(feed_specific_filename)
-        return feed_specific_filename
-      else
-        return File.join("templates", "default_items.rhtml")
-      end
-    end
-    
   end
+
+  # TODO: Is there a better name for this class?
+  class Lifer::Renderer
+
+    OUTPUT_FILENAME = "lifestream.html"
+
+    class << self
+      def render(items)
+        filename = File.join("templates", "default_items.rhtml")
+        content = render_erb_template(filename, binding)
+        wrap_feed(content)
+      end
+      
+      def wrap_feed(content)
+        filename = File.join("templates", "default_wrapper.rhtml")
+        output = render_erb_template(filename, binding)
+        string_to_file(output, OUTPUT_FILENAME)
+      end
+      
+      def render_erb_template(filename, outer_binding)
+        template = ""
+        File.open(filename, "r").each do |line|
+          template << line
+        end
+        erb = ERB.new(template)
+        erb.result(outer_binding)
+      end
+
+      def string_to_file(str, filename)
+        File.open(filename, "w") do |f|
+          f.write(str)
+        end
+      end
+    end
+
+  end
+
   
   class Lifer::Stream
+
+    NUM_OF_RESULTS = 20
    
     class << self 
       APP_CONFIG = YAML.load_file(File.join("config", "app_config.yml"))
@@ -59,28 +77,23 @@ module Lifer
       
       def generate
         content = ""
+        stream = []
         
         APP_CONFIG.each do |feed_name, config|
           user_config = USER_CONFIG["feeds"][feed_name]
-          feed = Lifer::Feed.new(feed_name, config["endpoint"], user_config)
-          content << feed.render
+          unless user_config.nil?
+            feed = Lifer::Feed.new(feed_name, config["endpoint"], user_config)
+            stream.concat(feed.get.items)
+          end
         end
-        
-        template = ""
-        File.open(File.join("templates", "default_wrapper.rhtml"), "r").each do |line|
-          template << line
-        end
-        erb = ERB.new(template)
-        output = erb.result(binding)
 
-        string_to_file(output, "lifestream.html")
+        stream.sort!{ |a, b| a.pubDate <=> b.pubDate }
+        stream.reverse!
+        stream = stream.slice(0,NUM_OF_RESULTS) # stream.slice! only left 5 items for some reason?!
+
+        Lifer::Renderer.render(stream)
       end
       
-      def string_to_file(str, filename)
-        File.open(filename, "w") do |f|
-          f.write(str)
-        end
-      end
     end
 
   end
